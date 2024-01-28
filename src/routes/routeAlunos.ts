e6e2bd1e7d5789bcb581c9d121f03d66eeb137fd
reinaldoper <reinaldoper83@gmail.com>
2024-01-28 14:44:01 -0400

    refactored: ajusta rota aluno

--- src/routes/routeAlunos.ts
+++ src/routes/routeAlunos.ts
@@ -6,11 +6,11 @@ const alunos = new AlunosController();
 
 const routeAluno = Router();
 
-routeAluno.get('/api/aluno', errorAluno, alunos.getAlunos);
+routeAluno.get('/api/aluno', alunos.getAlunos);
 routeAluno.get('/api/aluno/:id', errorIdAluno, alunos.getAlunoById)
 routeAluno.put('/api/aluno/:id', errorIdAluno, errorAlunoPut, alunos.updateAluno)
 routeAluno.delete('/api/aluno/:id', errorIdAluno, alunos.deleteAluno);
-routeAluno.post('/api/aluno', alunos.createAluno);
+routeAluno.post('/api/aluno', errorAluno, alunos.createAluno);
 
 
 
