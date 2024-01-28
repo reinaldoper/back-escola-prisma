482008fef8cd052ac11614fbbe5d6ecf8ec51a85
reinaldoper <reinaldoper83@gmail.com>
2024-01-28 14:50:45 -0400

    refactored: ajusta route

--- build/routes/routeAlunos.js
+++ build/routes/routeAlunos.js
@@ -8,9 +8,10 @@ const AlunosController_1 = __importDefault(require("../controllers/AlunosControl
 const validate_aluno_1 = require("../middlewares/validate-aluno");
 const alunos = new AlunosController_1.default();
 const routeAluno = (0, express_1.Router)();
-routeAluno.get('/api/aluno', validate_aluno_1.errorAluno, alunos.getAlunos);
+// alterado aqui
+routeAluno.get('/api/aluno', alunos.getAlunos);
 routeAluno.get('/api/aluno/:id', validate_aluno_1.errorIdAluno, alunos.getAlunoById);
 routeAluno.put('/api/aluno/:id', validate_aluno_1.errorIdAluno, validate_aluno_1.errorAlunoPut, alunos.updateAluno);
 routeAluno.delete('/api/aluno/:id', validate_aluno_1.errorIdAluno, alunos.deleteAluno);
-routeAluno.post('/api/aluno', alunos.createAluno);
+routeAluno.post('/api/aluno', validate_aluno_1.errorAluno, alunos.createAluno);
 exports.default = routeAluno;
