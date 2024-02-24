3e2c5e00028a1ffcf1d22e3c62d41f9abaf7af99
reinaldoper <reinaldoper83@gmail.com>
2024-02-24 17:47:32 -0400

    feat: adiciona tests professores

---
+++ tests/Professores.test.ts
@@ -0,0 +1,64 @@
+import { dataBoardTeachers, dataBoardTeacherId } from './mock/dataBoardTeachers';
+import chai from 'chai';
+import { Response } from 'superagent';
+import chaiHttp from 'chai-http';
+import app from '../src/index';
+const expect = chai.expect;
+
+
+chai.use(chaiHttp);
+let responseHttp: Response;
+
+describe('Testes da API Professores', () => {
+  it('Deve retornar lista de Professores.', async () => {
+    responseHttp = await chai
+      .request(app)
+      .get('/api/professor')
+      .send(dataBoardTeachers)
+    const { status, body } = responseHttp;
+    expect(status).to.be.equal(200);
+    expect(body.message).to.deep.equal(dataBoardTeachers);
+  });
+
+  it('Deve retornar status 404 para uma rota inexistente', (done) => {
+    chai.request(app)
+      .get('/rota-inexistente')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+
+  it('Deve retornar Professor.', async () => {
+    const id = 4
+    responseHttp = await chai
+      .request(app)
+      .get(`/api/professor/${id}`)
+      .send(dataBoardTeacherId)
+    const { status, body } = responseHttp;
+    expect(status).to.be.equal(200);
+    expect(body.message).to.deep.equal(dataBoardTeacherId);
+  });
+
+  it('Deve retornar Professor not Found.', async () => {
+    const id = 85
+    responseHttp = await chai
+      .request(app)
+      .get(`/api/professor/${id}`)
+
+    const { status, body } = responseHttp;
+    expect(status).to.be.equal(404);
+    expect(body.error).to.deep.equal("Teacher not found");
+  });
+
+  it('Deve retornar Invalid type id.', async () => {
+    const id = 'f'
+    responseHttp = await chai
+      .request(app)
+      .get(`/api/professor/${id}`)
+
+    const { status, body } = responseHttp;
+    expect(status).to.be.equal(400);
+    expect(body.error).to.deep.equal("Invalid id type");
+  });
+});
---
+++ tests/mock/dataBoardTeachers.ts
@@ -0,0 +1,143 @@
+export const dataBoardTeachers = [
+  {
+    "id": 2,
+    "nome": "Marcela das Neves",
+    "role": "ADMIN",
+    "disciplina": "matematica",
+    "email": "marcela_neves@school.com",
+    "createdAt": "2024-01-30T10:06:14.545Z",
+    "alunos": [
+      {
+        "id": 10,
+        "nome": "Marcos da Silva",
+        "idade": 23,
+        "email": "marcos@gmail.com",
+        "role": "USER",
+        "createdAt": "2024-02-15T16:29:24.776Z",
+        "professorId": 2
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "nome": "Joao Castelo Branco",
+    "role": "ADMIN",
+    "disciplina": "portugues",
+    "email": "joao_castelo@school.com",
+    "createdAt": "2024-01-30T11:00:47.682Z",
+    "alunos": [
+      {
+        "id": 5,
+        "nome": "Giovanna",
+        "idade": 24,
+        "email": "gi@email.com",
+        "role": "USER",
+        "createdAt": "2024-01-30T19:18:51.452Z",
+        "professorId": 4
+      },
+      {
+        "id": 6,
+        "nome": "Joao Paulo",
+        "idade": 45,
+        "email": "joao@email.com",
+        "role": "USER",
+        "createdAt": "2024-01-30T19:39:13.498Z",
+        "professorId": 4
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "nome": "Reinaldo Pereira",
+    "role": "ADMIN",
+    "disciplina": "programacao",
+    "email": "reinaldo@school.com",
+    "createdAt": "2024-01-30T11:24:18.954Z",
+    "alunos": [
+      {
+        "id": 7,
+        "nome": "Rafaela Silva",
+        "idade": 19,
+        "email": "rafa@gmail.com",
+        "role": "USER",
+        "createdAt": "2024-01-30T19:50:50.968Z",
+        "professorId": 5
+      },
+      {
+        "id": 8,
+        "nome": "Maria de Paula",
+        "idade": 25,
+        "email": "maria@hotmail.com",
+        "role": "USER",
+        "createdAt": "2024-01-30T20:00:41.301Z",
+        "professorId": 5
+      },
+      {
+        "id": 9,
+        "nome": "Matheus Henrique",
+        "idade": 23,
+        "email": "henrique@hotmail.com",
+        "role": "USER",
+        "createdAt": "2024-02-14T18:13:13.724Z",
+        "professorId": 5
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "nome": "Josefina Costantina",
+    "role": "ADMIN",
+    "disciplina": "historia",
+    "email": "josefuna@school.com",
+    "createdAt": "2024-01-30T11:26:26.807Z",
+    "alunos": [
+      {
+        "id": 3,
+        "nome": "João Pedro da Silva",
+        "idade": 21,
+        "email": "joaopedro@gmail.com",
+        "role": "USER",
+        "createdAt": "2024-01-28T11:53:59.410Z",
+        "professorId": 6
+      },
+      {
+        "id": 1,
+        "nome": "Pedro Paulo Diniz",
+        "idade": 19,
+        "email": "pedro@gmail.com",
+        "role": "USER",
+        "createdAt": "2024-01-28T11:50:03.077Z",
+        "professorId": 6
+      }
+    ]
+  }
+]
+
+export const dataBoardTeacherId = {
+  "id": 4,
+  "nome": "Joao Castelo Branco",
+  "role": "ADMIN",
+  "email": "joao_castelo@school.com",
+  "disciplina": "portugues",
+  "createdAt": "2024-01-30T11:00:47.682Z",
+  "alunos": [
+    {
+      "id": 5,
+      "nome": "Giovanna",
+      "idade": 24,
+      "email": "gi@email.com",
+      "role": "USER",
+      "createdAt": "2024-01-30T19:18:51.452Z",
+      "professorId": 4
+    },
+    {
+      "id": 6,
+      "nome": "Joao Paulo",
+      "idade": 45,
+      "email": "joao@email.com",
+      "role": "USER",
+      "createdAt": "2024-01-30T19:39:13.498Z",
+      "professorId": 4
+    }
+  ]
+}
